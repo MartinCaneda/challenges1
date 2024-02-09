57e5c536ad34114c2b564104a2998465d0edcf42
Martin <martincanedagz@gmail.com>
2024-02-09 16:03:54 +0100

    final

---
+++ js/track-your-savings/track.js
@@ -0,0 +1,11 @@
+const startingBalance = 1000;
+const monthlyContribution = 500;
+const monthPeriod = 12;
+
+totalSavings = startingBalance + monthlyContribution * monthPeriod;
+
+console.log(
+  "After 12 months of savings, you will have",
+  totalSavings,
+  "in your account"
+);
