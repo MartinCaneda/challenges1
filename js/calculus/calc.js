7f3f9a2b2f2e73ab3bb163f32cbba06d5f62e852
Martin <martincanedagz@gmail.com>
2024-02-09 16:21:14 +0100

    fix

---
+++ js/calculus/calc.js
@@ -0,0 +1,11 @@
+const circleRadius = 5;
+const circleArea = Math.PI * Math.pow(2.5, 2);
+const circleCircumference = 2 * Math.PI * 2.5;
+
+console.log(
+  "For a circle with a radius of 5 units: The area is approximately",
+  circleArea,
+  "The circumference is approximately",
+  circleCircumference,
+  "units"
+);
