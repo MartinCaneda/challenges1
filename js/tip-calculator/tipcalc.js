81bae0bbcd64e66bb15de70dc299dbb56a8f890f
Martin <martincanedagz@gmail.com>
2024-02-09 15:51:53 +0100

    final

---
+++ js/tip-calculator/tipcalc.js
@@ -0,0 +1,9 @@
+const mealCost = 15;
+const tipPercentage = 0.2;
+const tipAmmount = mealCost * tipPercentage;
+
+console.log("tipAmmount", tipAmmount);
+
+const totalCost = tipAmmount + mealCost;
+
+console.log("The total cost of your bill is", totalCost);
