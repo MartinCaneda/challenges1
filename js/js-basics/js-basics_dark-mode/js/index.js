770d507d9f4700730be8807285bc747da45414b4
Martin <martincanedagz@gmail.com>
2024-02-12 22:14:15 +0100

    dark-mode

--- js/js-basics/js-basics_dark-mode/js/index.js
+++ js/js-basics/js-basics_dark-mode/js/index.js
@@ -4,15 +4,15 @@ const bodyElement = document.querySelector('[data-js="body"]');
 
 const darkButton = document.querySelector('[data-js="dark-mode-button"]');
 darkButton.addEventListener("click", () => {
-  bodyElement.classList.add("dark");
+  body.classList.add("dark");
 });
 
 const lightButton = document.querySelector('[data-js="light-mode-button"]');
 lightButton.addEventListener("click", () => {
-  bodyElement.classList.remove("dark");
+  body.classList.remove("dark");
 });
 
 const toggleButton = document.querySelector('[data-js="toggle-button"]');
 toggleButton.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
+  body.classList.toggle("dark");
 });
