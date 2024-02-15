6c4fbc486ce70d67ca7d579d268749c19ecc5418
Martin <martincanedagz@gmail.com>
2024-02-15 13:55:54 +0100

    save commit

--- js/js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js/js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,13 +1,22 @@
 /*
 
-Change the appearance of the box according to the values of the sliders (color, border radius, rotation)
+Change the appearance of the box according to the values of
+the sliders (color, border radius, rotation)
 
 – [ ] Add an event listener for each input element.
-– [ ] When the slider control of inputColor is moved, change the background color of the box. Hint: You need to work with the hsl() functional notation for colors.
-– [ ] When the slider control of inputRadius is moved, change the border radius of the box. If the control reaches the right end, make the box look like a circle.
-– [ ] When the slider control of inputRotation is moved, rotate the box. Hint: You may need to work with the CSS property "transform" and a value that contains "rotate" and "deg".
+– [ ] When the slider control of inputColor is moved,
+change the background color of the box. Hint: You need
+to work with the hsl() functional notation for colors.
+– [ ] When the slider control of inputRadius is moved,
+change the border radius of the box. If the control reaches
+the right end, make the box look like a circle.
+– [ ] When the slider control of inputRotation is moved,
+rotate the box. Hint: You may need to work with the CSS
+property "transform" and a value that contains "rotate"
+and "deg".
 
-Hint: You can work with the property "style", just like you worked with the property "classList".
+Hint: You can work with the property "style",
+just like you worked with the property "classList".
 
 For further information check MDN:
 - about inputs of type="range": https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
@@ -16,3 +25,12 @@ For further information check MDN:
 - about CSS declarations with "style": https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
 
 */
+query selectors:
+box 
+colorinput tienevenetlistener con box.style.backgroundColor
+rotationinput tieneeventlistener con box.style.transform and box.style.width
+radiusinput tieneeventlistener con box.style.borderRadius
+Text
+
+
+
