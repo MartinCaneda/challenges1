b038f6efe1981a22c8b8547eb5ba6aa44b5959b9
Martin <martincanedagz@gmail.com>
2024-02-20 14:04:40 +0100

    list

--- js/js-loops/js-loops_forof-list/js/index.js
+++ js/js-loops/js-loops_forof-list/js/index.js
@@ -5,7 +5,7 @@ const main = document.querySelector('[data-js="main"]');
 // Create a list from an array
 
 const ol = document.createElement("ol");
-main.append(ol);
+/* main.append(ol); */
 
 const programmingLanguages = [
   "JavaScript",
@@ -18,5 +18,12 @@ const programmingLanguages = [
 ];
 
 // --v-- write/change code here --v--
+for (const list of programmingLanguages) {
+  const li = document.createElement("li");
+  li.textContent = list;
 
+  main.appendChild(li);
+}
+
+main.appendChild(ol);
 // --^-- write/change code here --^--
