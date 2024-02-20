b2c9601f0b3c953f736394fb9f8fd57336519f8b
Martin <martincanedagz@gmail.com>
2024-02-20 17:21:59 +0100

    stars

--- js/js-loops/js-loops_stars/js/index.js
+++ js/js-loops/js-loops_stars/js/index.js
@@ -2,12 +2,23 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-
+  for (let counter = 1; counter <= 5; counter++) {
+    const myImage = document.createElement("img");
+    if (filledStars >= counter) {
+      myImage.src = "assets/star-filled.svg";
+    } else {
+      myImage.src = "assets/star-empty.svg";
+    }
+    myImage.addEventListener("click", function () {
+      renderStars(counter);
+    });
+    starContainer.appendChild(myImage);
+  }
   //--^-- your code here --^--
 }
 
