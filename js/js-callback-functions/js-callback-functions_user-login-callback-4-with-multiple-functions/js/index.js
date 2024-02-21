1b4f68a5275e52f3db2bbcb18406ece5405b7c10
Martin <martincanedagz@gmail.com>
2024-02-21 11:44:17 +0100

    quote change

--- js/js-callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js/js-callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -10,11 +10,11 @@ function handleUserLogin(onSuccess, onError, userName) {
 
 // The exercise starts here!
 function showWelcomeMessage(userName, userRole) {
-  console.log("Welcome ${userName}! You are logged in now as ${userRole}.");
+  console.log(`"Welcome ${userName}! You are logged in now as ${userRole}."`);
 }
 
 function showErorMessage(errorMessage) {
-  console.log("Login error: ${errorMessage}");
+  console.log(`"Login error: ${errorMessage}"`);
 }
 
 handleUserLogin(showWelcomeMessage, showErorMessage, "Jane Doe");
