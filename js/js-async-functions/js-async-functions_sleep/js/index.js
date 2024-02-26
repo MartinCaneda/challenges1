7293aaaad005824dab1203ea80088cd1c37f52e5
Martin <martincanedagz@gmail.com>
2024-02-26 11:45:03 +0100

    added dark mode

--- js/js-async-functions/js-async-functions_sleep/js/index.js
+++ js/js-async-functions/js-async-functions_sleep/js/index.js
@@ -1,3 +1,33 @@
+/* We have 3 buttons which should display a sleep emoji for 3, 
+5 and 10 seconds. After the time has passed, the emoji 
+should be replaced by the alarm clock. But all buttons 
+display the alarm clock immediately.
+
+### Button 1
+
+- Fix the bug of this first button. It has something to do 
+with the sleep function. It returns a Promise...
+
+### Button 2
+
+- Use the sleep function here to display the sleep emoji 
+for 5 seconds.
+
+### Button 3
+
+- This time we use a named function. Add the same f
+unctionality as with button 2, but display the emoji for 
+10 seconds.
+
+### Extra: Dark Mode
+
+Nobody can sleep with this bright display. Each button 
+should also add the class "dark" to the `body` for the 
+respective amount of time - and remove it afterwards.
+
+> 💡 Hint: You don't need to add any css, the dark class 
+is already written */
+
 const icon = document.querySelector('[data-js="icon"]');
 const button3 = document.querySelector('[data-js="sleep-3-button"]');
 const button5 = document.querySelector('[data-js="sleep-5-button"]');
@@ -7,19 +37,27 @@ const body = document.body;
 // --v-- code here --v--
 
 button3.addEventListener("click", async () => {
+  body.classList.add("dark");
   icon.textContent = "💤";
-  sleep(3);
+  await sleep(3);
   icon.textContent = "⏰";
+  body.classList.remove("dark");
 });
 
-button5.addEventListener("click", () => {
+button5.addEventListener("click", async () => {
+  body.classList.add("dark");
   icon.textContent = "💤";
+  await sleep(5);
   icon.textContent = "⏰";
+  body.classList.remove("dark");
 });
 
-function handleSleepFor10Button() {
+async function handleSleepFor10Button() {
+  body.classList.add("dark");
   icon.textContent = "💤";
+  await sleep(10);
   icon.textContent = "⏰";
+  body.classList.remove("dark");
 }
 
 button10.addEventListener("click", handleSleepFor10Button);
