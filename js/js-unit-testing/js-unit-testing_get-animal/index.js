94bcefbe24a878d76987a67d8bae3db53ef97845
Martin <martincanedagz@gmail.com>
2024-02-28 10:45:19 +0100

    final first ex

--- js/js-unit-testing/js-unit-testing_get-animal/index.js
+++ js/js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animalName) {
+  if (animalName === "cats") {
+    return "I totally love cats!";
+  } else if (animalName) {
+    return `I like ${animalName}!`;
+  } else {
+    return "I do not like animals at all!";
+  }
+}
