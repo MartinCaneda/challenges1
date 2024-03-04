39caa79a47a9771f450147f435e8cbf32dfc92b3
Martin <martincanedagz@gmail.com>
2024-03-04 15:29:51 +0100

    final

--- react/react-basics/react-basics_attributes/src/App.jsx
+++ react/react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Some Text</h2>
+      <label htmlFor="labelInput">Input:</label>
+      <input type="text" id="labelInput" />
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/TypeScript"
+      >
+        Typescript
+      </a>
+    </article>
+  );
 }
