bba40663051ac6b424a1b5ac61951c0071a1bace
Martin <martincanedagz@gmail.com>
2024-03-05 10:21:54 +0100

    added age

--- react/react-props/react-props_greeting/src/App.jsx
+++ react/react-props/react-props_greeting/src/App.jsx
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Martin" age={33} pob="O Grove" />;
+}
+
+function Greeting({ name, age, pob }) {
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      <h2>Edad: {age}</h2>
+      <h2>El pueblo de {pob}</h2>
+    </div>
+  );
 }
