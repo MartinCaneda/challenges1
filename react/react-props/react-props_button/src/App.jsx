543aa90756b17a6bcc552ac386a610e306c95838
Martin <martincanedagz@gmail.com>
2024-03-05 11:35:33 +0100

    last

--- react/react-props/react-props_button/src/App.jsx
+++ react/react-props/react-props_button/src/App.jsx
@@ -1,5 +1,30 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  const handleClick = () => {
+    console.log("Clickity click click!");
+  };
+
+  return (
+    <Button
+      color="blue"
+      disabled={false}
+      text="click on me"
+      onClick={handleClick}
+    />
+  );
+}
+
+function Button({ color, disabled, text, onClick }) {
+  const buttonStyle = {
+    color: color,
+  };
+
+  return (
+    <div>
+      <button style={buttonStyle} disabled={disabled} onClick={onClick}>
+        {text}
+      </button>
+    </div>
+  );
 }
