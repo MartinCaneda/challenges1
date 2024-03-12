c5e1ca7ef72bbc0ef05ad0c4260eab1934818d2d
Martin <martincanedagz@gmail.com>
2024-03-12 16:18:36 +0100

    last

--- react/react-with-local-storage/react-with-local-storage_dice/src/components/D6Button/index.jsx
+++ react/react-with-local-storage/react-with-local-storage_dice/src/components/D6Button/index.jsx
@@ -1,20 +1,16 @@
-import { useState } from "react";
 import { getD6Roll } from "../../utils";
 import D6 from "../D6";
 import "./D6Button.css";
 
-export function D6Button() {
-  const [rolls, setRolls] = useState([]);
-
+export function D6Button({ onRoll }) {
   const handleRoll = () => {
-    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+    const newRoll = { value: getD6Roll(), time: Date.now() };
+    onRoll(newRoll);
   };
-
-  const currentRollValue = rolls[0]?.value;
 
   return (
     <button className="d6-button" type="button" onClick={handleRoll}>
-      <D6 value={currentRollValue} />
+      <D6 />
     </button>
   );
 }
