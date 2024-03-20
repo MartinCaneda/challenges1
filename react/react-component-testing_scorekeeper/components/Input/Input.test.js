bfec31fce30f6f8548eab7f3e8cc70d2b8726f70
Martin <martincanedagz@gmail.com>
2024-03-20 14:19:40 +0100

    mock function added on input component

--- react/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,24 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input labelText="Name of game" placeholder="Dodelido" />);
+  const labelText = screen.getByText(/Name of game/i);
+  const placeholder = screen.getByPlaceholderText(/Dodelido/i);
+  expect(labelText).toBeInTheDocument();
+  expect(placeholder).toBeInTheDocument();
+});
 
-test("calls callback on every user input", async () => {});
+//Mock situation
+test("calls callback on every user input", async () => {
+  const onChange = jest.fn();
+  const user = userEvent.setup();
+
+  render(<Input onChange={onChange}> </Input>);
+
+  const input = screen.getByRole("textbox");
+
+  await user.type(input, "my game");
+
+  expect(onChange).toHaveBeenCalledTimes(7);
+});
--- react/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
+++ react/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Navigation from ".";
+import Link from "next/link";
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -7,4 +8,10 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders with two links 'Play' and 'History'", () => {});
+test("renders with two links 'Play' and 'History'", () => {
+  render(<Navigation></Navigation>);
+  const playLink = screen.getByRole("link", { name: "Play" });
+  expect(playLink).toBeInTheDocument();
+  const historyLink = screen.getByRole("link", { name: "History" });
+  expect(historyLink).toBeInTheDocument();
+});
