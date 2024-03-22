c0d3057a5d5f8a0c79e6f92cf8cc027baa7cbcb8
Martin <martincanedagz@gmail.com>
2024-03-22 11:41:26 +0100

    changed setCars to updateCars and applied useImmer

--- react/react-immutable-state/react-immutable-state_car-race/components/CarRace/index.js
+++ react/react-immutable-state/react-immutable-state_car-race/components/CarRace/index.js
@@ -8,24 +8,32 @@ import {
   Track,
 } from "./CarRace.styled";
 import { initialCars, getRandomDistance } from "../../utils/utils";
+import { useImmer } from "use-immer";
 
 const finishLine = 200;
 
 export default function CarRace() {
-  const [cars, setCars] = useState(initialCars);
+  const [cars, updateCars] = useImmer(initialCars);
 
   function moveCar(clickedCar) {
     const coveredDistance = getRandomDistance();
-    console.log("clickedCar", clickedCar);
-    console.log("coveredDistance", coveredDistance);
-  }
 
+    updateCars((draft) => {
+      const clickedCarIndex = draft.findIndex(
+        (car) => car.emoji === clickedCar.emoji
+      );
+      if (clickedCarIndex !== -1) {
+        draft[clickedCarIndex].position.x += coveredDistance;
+        draft[clickedCarIndex].position.lastDistance = coveredDistance;
+      }
+    });
+  }
   const winner = cars.find((car) => car.position.x >= finishLine);
 
   return (
     <>
       {winner ? (
-        <Winner winner={winner} onRestart={() => setCars(initialCars)} />
+        <Winner winner={winner} onRestart={() => updateCars(initialCars)} />
       ) : (
         <AllCarRoutes $finishLine={finishLine}>
           <DistanceHeadline>Last Distance</DistanceHeadline>
